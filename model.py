ec51ab6978d69fe3385c3ad1ca538176f46adf6d
hermit <gucciqqting@126.com>
2019-03-18 22:37:30 +0800

    增加模型resnet_talnn’

--- model.py
+++ model.py
@@ -2,17 +2,57 @@
 import torch
 from torch import nn
 
-from models import talnn
+from models import talnn,resnet_talnn
 
 def generate_model(opt):
     assert opt.model in [
-        'talnn'
+        'talnn','resnet_talnn'
     ]
     if opt.model == 'talnn':
         from models.talnn import get_fine_tuning_parameters
         model = talnn.TALNN(
                     sample_size=opt.sample_size,
                     sample_duration=opt.sample_duration)
+    elif opt.mode = 'resnet_talnn':
+        assert opt.model_depth in [10, 18, 34, 50, 101, 152, 200]
+        from models.resnet_talnn import get_fine_tuning_parameters
+        if opt.model_depth == 10:
+            model = resnet_talnn.resnet_talnn10(
+                shortcut_type=opt.resnet_shortcut,
+                sample_size=opt.sample_size,
+                sample_duration=opt.sample_duration)
+        elif opt.model_depth == 18:
+            model = resnet_talnn.resnet_talnn18(
+                shortcut_type=opt.resnet_shortcut,
+                sample_size=opt.sample_size,
+                sample_duration=opt.sample_duration)
+        elif opt.model_depth == 34:
+            model = resnet_talnn.resnet_talnn34(
+                shortcut_type=opt.resnet_shortcut,
+                sample_size=opt.sample_size,
+                sample_duration=opt.sample_duration)
+        elif opt.model_depth == 50:
+            model = resnet_talnn.resnet_talnn50(
+                shortcut_type=opt.resnet_shortcut,
+                sample_size=opt.sample_size,
+                sample_duration=opt.sample_duration)
+        elif opt.model_depth == 101:
+            model = resnet_talnn.resnet_talnn101(
+                shortcut_type=opt.resnet_shortcut,
+                sample_size=opt.sample_size,
+                sample_duration=opt.sample_duration)
+        elif opt.model_depth == 152:
+            model = resnet_talnn.resnet_talnn152(
+                shortcut_type=opt.resnet_shortcut,
+                sample_size=opt.sample_size,
+                sample_duration=opt.sample_duration)
+        elif opt.model_depth == 200:
+            model = resnet_talnn.resnet_talnn200(
+                shortcut_type=opt.resnet_shortcut,
+                sample_size=opt.sample_size,
+                sample_duration=opt.sample_duration)
+    
+    
     if not opt.no_cuda:
         model = model.cuda()
     
